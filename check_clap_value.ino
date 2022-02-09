5ed1f49c4af45d7b9abd72272b5883cb93467f2c
cindy <mianalaloharano@gmail.com>
2022-02-09 12:50:28 +0300

    working well for me

--- check_clap_value.ino
+++ check_clap_value.ino
@@ -1,69 +1,52 @@
-#define signalToRelayPin 13
+#define togglePin 13
 #define sensorPin 2
 
-int lastSoundValue;
-int soundValue;
-long lastNoiseTime = 0;
-long currentNoiseTime = 0;
-long lastLightChange = 0;
-int relayStatus = HIGH;
+int time = 0;
+int timeDifference = 0;
+int previousTime = 0;
 
 boolean val = 0;
 boolean newVal = 0;
+boolean currentState = 0;
 
 void setup()
 {
   pinMode(sensorPin, INPUT);
-  pinMode(signalToRelayPin, OUTPUT);
+  pinMode(togglePin, OUTPUT);
   Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
 void loop()
 {
   newVal = digitalRead(sensorPin);
+
+  // the value has changed
   if (val != newVal)
   {
     val = newVal;
+
+    // this can trigger four times in a span of 6 milliseconds
     if (val == 1)
     {
-      Serial.println("Flick");  // wait for a second
-      Serial.println(millis()); // wait for a second
+
+      time = millis();
+      timeDifference = time - previousTime;
+      if (timeDifference > 10)
+      {
+
+        // Serial.println("Flick");
+        // Serial.println(millis());
+
+        // the time delay will be between 100 and 800 milliseconds
+        if (timeDifference > 100 && timeDifference < 800)
+        {
+          Serial.println("Activated");
+          currentState = digitalRead(togglePin);
+          digitalWrite(togglePin, !currentState);
+        }
+      }
+
+      previousTime = time;
     }
   }
 }
-
-// void loop()
-// {
-//   soundValue = digitalRead(sensorPin);
-//   currentNoiseTime = millis();
-
-//   if (soundValue == 1)
-//   { // if there is currently a noise
-
-//     if (
-//         (currentNoiseTime > lastNoiseTime + 200) // to debounce a sound occurring in more than a loop cycle as a single noise
-//         && (lastSoundValue == 0)                     // if it was silent before
-//         // && (currentNoiseTime < lastNoiseTime + 800)  // if current clap is less than 0.8 seconds after the first clap
-//         // && (currentNoiseTime > lastLightChange + 1000) // to avoid taking a third clap as part of a pattern
-//     )
-//     {
-//       Serial.println("Yeah!");
-//       relayStatus = !relayStatus;
-//       digitalWrite(signalToRelayPin, relayStatus);
-//       lastLightChange = currentNoiseTime;
-//       delay(500);
-//     }else{
-//       Serial.println("Current");
-//       Serial.println(currentNoiseTime);
-//       Serial.println("Last");
-//       Serial.println(lastNoiseTime);
-
-//       digitalWrite(signalToRelayPin, LOW);
-//     }
-
-//     lastNoiseTime = currentNoiseTime;
-//   }
-
-//   lastSoundValue = soundValue;
-// }
